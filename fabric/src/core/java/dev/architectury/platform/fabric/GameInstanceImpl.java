291e84fde45c38bfdf3171479acfc21e257389de
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-07-15 00:23:19 +1000

    move the core impls to core project

--- fabric/src/core/java/dev/architectury/platform/fabric/GameInstanceImpl.java
+++ fabric/src/core/java/dev/architectury/platform/fabric/GameInstanceImpl.java
@@ -17,7 +17,7 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-package dev.architectury.utils.fabric;
+package dev.architectury.platform.fabric;
 
 import dev.architectury.event.EventHandler;
 import dev.architectury.event.events.common.LifecycleEvent;
