b30e1da35df747ca8c1e662ccc984c610fcfcafe
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-07-15 16:28:31 +1000

    more tweaks

--- buildSrc/src/main/groovy/dev/architectury/buildsrc/ProjectModule.java
+++ buildSrc/src/main/groovy/dev/architectury/buildsrc/ProjectModule.java
@@ -43,6 +43,7 @@ public record ProjectModule(BuildSrcExtension extension, String name, Reference<
                 }
             }
         }
+        return strings;
     }
     
     public HashSet<ProjectModule> getTransitiveDependencies() {
